455c577a440e8388efe39489f74e310db00a8d9e
Kien Nguyen <kdot@Kiens-MacBook-Pro.local>
2018-09-02 14:03:53 -0700

    add service worker function in main js file

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ let restaurants,
 var newMap
 var markers = []
 
+
+/* Service Worker*/
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('sw/sw.js')
+  .catch(function(err) {
+    console.error(err);
+  });
+}
+
+
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
